9c3c215279b6a866f8f95e0b5f573729c213cea5
w-q-kang <wolf.kang@web.de>
2023-08-12 15:38:08 +0200

    windows make file added.

---
+++ make.bat
@@ -0,0 +1,7 @@
+mkdir bin
+mkdir obj
+mkdir obj\src
+g++.exe -Wall -fexceptions -g -O2 -std=c++17 -Og -Iinclude -c main.cpp -o obj\main.o
+g++.exe -Wall -fexceptions -g -O2 -std=c++17 -Og -Iinclude -c src\Basics.cpp -o obj\src\Basics.o
+g++.exe -Wall -fexceptions -g -O2 -std=c++17 -Og -Iinclude -c src\Cell.cpp -o obj\src\Cell.o
+g++.exe  -o bin\NORG.exe obj\main.o obj\src\Basics.o obj\src\Cell.o  -Wl,--stack,25000000 
